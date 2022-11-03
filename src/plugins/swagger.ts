e0ee7ad69a0d3dd13ca669172f67c587abf1d182
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-03 11:03:03 +0100

    Changed swagger options for version 8

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -3,5 +3,44 @@ import swagger, { FastifyDynamicSwaggerOptions } from '@fastify/swagger';
 
 
 export default fp<FastifyDynamicSwaggerOptions>(async (app) => {
-    app.register(swagger);
+    app.register(
+        swagger, <FastifyDynamicSwaggerOptions>{
+            mode: 'dynamic',
+            openapi: {
+                info: {
+                    title: 'REMONDIS API',
+                    description: 'Remondis API',
+                    version: '1.0.0'
+                },
+                externalDocs: {
+                    url: 'https://swagger.io',
+                    description: 'Find more info here'
+                },
+                host: 'localhost',
+                schemes: ['http'],
+                consumes: ['application/json'],
+                produces: ['application/json'],
+                tags: [
+                    { name: 'user', description: 'User related end-points' },
+                    { name: 'case', description: 'Case related end-points' },
+                    { name: 'appointment', description: 'Appointment related end-points' },
+                ],
+            }
+        });
 })
+
+/*
+mode: 'dynamic',
+  openapi: {
+    info: {
+      title: String,
+      description: String,
+      version: String,
+    },
+    externalDocs: Object,
+    servers: [ Object ],
+    components: Object,
+    security: [ Object ],
+    tags: [ Object ]
+  }
+*/
--- src/plugins/swagger_ui.ts
+++ src/plugins/swagger_ui.ts
@@ -6,34 +6,28 @@ export default fp<FastifySwaggerUiOptions>(async (app) => {
 
     app.register(
         swaggerUi, <FastifySwaggerUiOptions>{
-            info: {
-                title: 'REMONDIS API',
-                description: 'Remondis API',
-                version: '0.1.0'
+            uiConfig: {
+                //docExpansion: 'full',
+                deepLinking: false
             },
-            externalDocs: {
-                url: 'https://swagger.io',
-                description: 'Find more info here'
-            },
-            host: 'localhost',
-            schemes: ['http'],
-            consumes: ['application/json'],
-            produces: ['application/json'],
-            tags: [
-                { name: 'user', description: 'User related end-points' },
-                { name: 'case', description: 'Case related end-points' }
-            ],
-            // securityDefinitions: {
-            //     apiKey: {
-            //         type: 'apiKey',
-            //         name: 'apiKey',
-            //         in: 'header'
-            //     }
-            // }
             hideUntagged: true,
-            exposeRoute: true,
-            routePrefix: '/docs'
-
+            routePrefix: '/docs',
+            staticCSP: true,
         })
 
 })
+
+/* 
+routePrefix: '/documentation',
+  initOAuth: { },
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) { next() },
+    preHandler: function (request, reply, next) { next() }
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header 
+  */
