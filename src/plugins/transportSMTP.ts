4a51ebd79828431cbf6af05733ca3247fc25906b
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-08 22:14:31 +0100

    Added sendEmail example

--- src/plugins/transportSMTP.ts
+++ src/plugins/transportSMTP.ts
@@ -19,6 +19,16 @@ const transportPlugin: FastifyPluginAsync = fp(async (server, options) => {
     // Make SMTP transport available through the fastify server instance: server.transporter
     server.decorate('transporter', transporter)
 
+    /*
+    // EXAMPLE: send mail with defined transport object
+    const info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <noreply@remondis.com>', // sender address
+        to: "user@remondis.com", // list of receivers
+        subject: "Please verify your email address", // Subject line
+        text: "", // plain text body
+        html: "", // html body
+    });
+     */
 })
 
 export default transportPlugin;
