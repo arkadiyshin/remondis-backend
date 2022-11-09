bb167aa651814804764c306911a417d25083b18a
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-09 23:07:23 +0100

    style(conf): remove comments in configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -13,10 +13,4 @@ const getEnvValue = (keyname : string) : any => {
 export const APP_PORT = getEnvValue('APP_PORT');
 export const DATABASE_URL = getEnvValue('DATABASE_URL');
 export const JWT_SECRET_KEY = getEnvValue('JWT_SECRET_KEY');
-
-// export const SMTP_HOST = getEnvValue('SMTP_HOST');
-// export const SMTP_PORT = getEnvValue('SMTP_PORT');
-// export const SMTP_USER = getEnvValue('SMTP_USER');
-// export const SMTP_PASS = getEnvValue('SMTP_PASS');
-
 export const SENDGRID_API_KEY = getEnvValue('SENDGRID_API_KEY');
