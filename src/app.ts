90b9dd2192536b39b633f576ba148b4900712f45
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-11 23:38:22 +0100

    refactor(general): convert app from instance to plugin

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,36 @@
 import path, { join } from "path";
 import { fileURLToPath } from "url";
-import Fastify, { FastifyInstance } from "fastify";
-
+import { FastifyPluginAsync } from "fastify";
 
 // read about autoload options here https://github.com/fastify/fastify-autoload
-import autoLoad from "@fastify/autoload";
+import autoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
 
 // this code fixed error: "__dirname is not defined in ES module scope"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app: FastifyInstance = Fastify({
-  logger: {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        destination: 1,
-        colorize: true,
-        translateTime: "HH:MM:ss.l",
-        ignore: "pid,hostname",
-      },
-    },
-  },
-});
+
+export type AppOptions = {
+  
+} & Partial<AutoloadPluginOptions>;
 
 
-app.register(autoLoad, {
-  dir: join(__dirname, "plugins"),
-});
+const app: FastifyPluginAsync<AppOptions> = async (
+  fastify,
+  opts
+): Promise<void> => {
 
-app.register(autoLoad, {
-  dir: join(__dirname, "routes"),
-  routeParams: true
-});
+  fastify.register(autoLoad, {
+    dir: join(__dirname, "plugins"),
+    options: opts
+  });
+
+  fastify.register(autoLoad, {
+    dir: join(__dirname, "routes"),
+    options: opts,
+    routeParams: true
+  });
+
+};
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
+import Fastify, { FastifyInstance } from "fastify";
 import app from './app.js';
-import {APP_PORT} from "./configuration.js";
+import { APP_PORT } from "./configuration.js";
 
+const server: FastifyInstance = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        destination: 1,
+        colorize: true,
+        translateTime: "HH:MM:ss.l",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+});
+
+server.register(app, {} );
 
 // Run the server!
 const port: number = parseInt(APP_PORT!) || 3000;
 const start = async () => {
   try {
-    await app.listen({ port: port });
+    await server.listen({ port: port });
   } catch (err) {
-    app.log.error(err);
+    server.log.error(err);
     process.exit(1);
   }
 };
